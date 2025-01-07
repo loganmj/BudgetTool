bdb7bf437a1a22a0abd4b5e7282960b47236e606
Michael <michaellogan7860@gmail.com>
2025-01-07 10:24:14 -0600

    Added skeleton for monthly budget component.

---
+++ budgettool.client/src/app/monthly-budget/monthly-budget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-budget',
+  standalone: false,
+  
+  templateUrl: './monthly-budget.component.html',
+  styleUrl: './monthly-budget.component.css'
+})
+export class MonthlyBudgetComponent {
+
+}
